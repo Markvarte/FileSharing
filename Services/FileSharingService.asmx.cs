15597be4dc3753852a22eefcd840d92739d2c2f0
Natalija Markvarte <natalija.markvarte@bbit.tech>
2021-04-04 16:59:03 +0300

    Added sent mail and while xml log file logic to Service.

--- Services/FileSharingService.asmx.cs
+++ Services/FileSharingService.asmx.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Services;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
+using System.Xml;
 
 namespace fileSharing.Services
 {
@@ -19,6 +23,9 @@ namespace fileSharing.Services
     // [System.Web.Script.Services.ScriptService]
     public class FileSharingService : System.Web.Services.WebService
     {
+        IPAddress globalIp = null;
+        string globalUploadedDate = "";
+        string globalUploadedTime = "";
 
         [WebMethod]
         public string CreateSharing(HtmlInputFile oFile, string pw, TextBox eMail = null, TextBox comment = null)
@@ -28,12 +35,31 @@ namespace fileSharing.Services
                 return "Wrong password! Access denied.";
 
             }
-            return UploadFile(oFile);
+            string resString;
+            string sharingLink = UploadFile(oFile);
+            resString = sharingLink;
+            if (eMail != null && sharingLink.StartsWith("http"))
+            {
+                string mailResult = SentMail(eMail, comment, sharingLink);
+                resString = $"Sent mail result: {mailResult}, sharing link: {sharingLink}";
+            }
+
+            LogInfo loginfo = new LogInfo {
+                sharingLink = sharingLink,
+                uploadedFileName = oFile.PostedFile.FileName,
+                ipAddress = "localhost",
+                eMail = eMail.Text,
+                uploadDate = globalUploadedDate,
+                uploadedTime = globalUploadedTime };
+
+            WriteLogInformation(loginfo);
+            return resString;
+
         }
         [WebMethod]
         public string UploadFile(HtmlInputFile oFile)
         {
-
+            string sharingLink;
             string strFileName;
             string strFilePath;
             string strFolder;
@@ -58,11 +84,14 @@ namespace fileSharing.Services
                 else
                 {
                     oFile.PostedFile.SaveAs(strFilePath);
+
+                    // Returning link to download file
                     // For axample http://haring.azurewebsites.net/api/File/202104031232546682
-                    // Comment
                     var isAzure = Environment.GetEnvironmentVariable("IsAzure") == "1";
                     var host = isAzure ? "http://haring.azurewebsites.net" : "https://localhost:44399";
-                    return $"{host}/api/File/{timeStamp}";
+
+                    sharingLink = $"{host}/api/File/{timeStamp}";
+                    return sharingLink;
                 }
             }
             else
@@ -70,14 +99,66 @@ namespace fileSharing.Services
                 return "Click 'Browse' to select the file to upload.";
             }
         }
-        [WebMethod]
-        public string CreateLinkForUploadedFile()
+        private void WriteLogInformation(LogInfo logInfo)
         {
-            return "Hello World";
+            StringBuilder sbuilder = new StringBuilder();
+            using (StringWriter sw = new StringWriter(sbuilder))
+            {
+                using (XmlTextWriter w = new XmlTextWriter(sw))
+                {
+                    w.WriteStartElement("LogInfo");
+                    w.WriteElementString("sharingLink", logInfo.sharingLink);
+                    w.WriteElementString("uploadedFileName", logInfo.uploadedFileName);
+                    w.WriteElementString("ipAddress", logInfo.ipAddress);
+                    w.WriteElementString("eMail", logInfo.eMail);
+                    w.WriteElementString("uploadDate", logInfo.uploadDate);
+                    w.WriteElementString("uploadedTime", logInfo.uploadedTime);
+                    w.WriteEndElement();
+                }
+            }
+
+            string strFolder = Server.MapPath($"./Logs/");
+            // Create the directory if it does not exist.
+            if (!Directory.Exists(strFolder))
+            {
+                Directory.CreateDirectory(strFolder);
+            }
+            string logFilePath = Server.MapPath($"./Logs/{DateTime.Now.ToString("yyyyMMddHHmmssffff")}.xml");
+            using (StreamWriter w = new StreamWriter(logFilePath, false, Encoding.UTF8))
+            {
+                w.WriteLine(sbuilder.ToString());
+            }
         }
 
-        static string GetTimestamp(DateTime value)
+        public string SentMail(TextBox email, TextBox comment, string sharinklink)
         {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(email.Text);
+                mail.From = new MailAddress("ASPNETwebapp@user.lv");
+                mail.Subject = "Sharing Link";
+                mail.Body = $"{comment.Text}\n{sharinklink}";
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "mail.hronoss.com";
+                smtp.Port = 2525;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("studenti@hronoss.com", "TestaPasts1");
+                smtp.EnableSsl = false;
+
+                smtp.Send(mail);
+                return "Mail Send succcesfully";
+            }
+            catch (Exception ex)
+            {                         
+                return ex.Message;
+            }
+        }
+
+        string GetTimestamp(DateTime value)
+        {
+            globalUploadedDate = value.ToString("yyyy-MM-dd");
+            globalUploadedTime = value.ToString("HH:mm:ss");
             return value.ToString("yyyyMMddHHmmssffff");
         }
 
---
+++ Services/LogInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fileSharing.Services
+{
+    public class LogInfo
+    {
+        public string sharingLink { get; set; }
+        public string uploadedFileName { get; set; }
+        public string ipAddress { get; set; }
+        public string eMail { get; set; }
+        public string uploadDate { get; set; }
+        public string uploadedTime { get; set; }
+    }
+}
